5fd059630e7ebfd4a28e6af950d29dba2a706a64
Thomas Whitford <twhitford2224@gmail.com>
2024-10-26 14:50:37 +0100

    Start

---
+++ test.py
@@ -0,0 +1,12 @@
+from manim import *
+
+
+class TransformCycle(Scene):
+    def construct(self):
+        a = Circle()
+        t1 = Square()
+        t2 = Triangle()
+        self.add(a)
+        self.wait()
+        for t in [t1,t2]:
+            self.play(Transform(a,t))
